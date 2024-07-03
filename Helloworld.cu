33d43512788a7fd99b85b671b68831f797a640e9
Pouria Hasani <hx298@catcomp01.ziti.uni-heidelberg.de>
2024-07-03 06:14:22 +0200

    Added Hellowrld!

--- Helloworld.cu
+++ Helloworld.cu
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 __global__ void mykernel(){
+  int idx = threadIdx.x;
+  printf("\n Hello from thread number %d",idx);
 }
 
 int main(){
 
-  mykernel<<<1,1>>>();
-  printf("\nCalled GPU kernel!");
+  mykernel<<<1,2>>>();
+  cudaDeviceSynchronize();
   return 0;
 }
